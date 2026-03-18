be2af8fb7e5f8931ae6b3dd0a9e56598f77fc425
karishma03012003 <karishma96529@gmail.com>
2026-03-19 00:58:58 +0530

    hi

--- prox-app/src/App.jsx
+++ prox-app/src/App.jsx
@@ -7,7 +7,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/git_app">
       <div className="app-container">
         <div className="mobile-frame">
           <Routes>
